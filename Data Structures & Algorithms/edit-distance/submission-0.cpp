3bf5b6956a547270553ce3e19013b11d7cac9da8
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2026-05-29 20:32:31 +0530

    Add: edit-distance - submission-0

---
+++ Data Structures & Algorithms/edit-distance/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int solve(int i, int j, string word1, string word2, vector<vector<int>>&dp){
+        if(i == word1.size()) return word2.size() - j;
+        if(j == word2.size()) return word1.size() - i;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(word1[i] == word2[j]){
+            return solve(i + 1, j + 1, word1, word2, dp);
+        }
+        int replace, insert, del;
+        if(word1[i] != word2[j]){
+            replace = 1 + solve(i + 1, j + 1, word1, word2, dp);
+            insert = 1 + solve(i, j+1, word1, word2, dp);
+            del = 1 + solve(i + 1, j, word1, word2, dp);
+        }
+        return dp[i][j] = min(replace, min(insert, del));
+    }
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>>dp(n, vector<int>(m, -1));
+        int ans = solve(0, 0, word1, word2, dp);
+        return ans;
+    }
+};
