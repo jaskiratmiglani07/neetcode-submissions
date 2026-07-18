0bb2c6276e51a6639890e221e42f7dff578856f8
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2026-07-18 17:32:46 +0530

    Add: balanced-binary-tree - submission-3

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-3.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    // bool height_balanced = true;
+    // int dfs(TreeNode* root){
+    //     if(!root || !height_balanced) return 0;
+    //     //get the lengths of the left and the right subtrees abs difference <= 1
+    //     int left = dfs(root->left);
+    //     int right = dfs(root->right);
+    //     if(abs(left - right) > 1){
+    //         height_balanced = false;
+    //     }
+    //     return 1 + max(left, right);
+    // }
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
+
+        int left = dfs(root->left);
+        if (left == -1) return -1;   // early exit
+
+        int right = dfs(root->right);
+        if (right == -1) return -1;  // early exit
+
+        if (abs(left - right) > 1)
+            return -1;
+
+        return 1 + max(left, right);
+    }
+    bool isBalanced(TreeNode* root) {
+        return dfs(root) != -1;
+    }
+};
