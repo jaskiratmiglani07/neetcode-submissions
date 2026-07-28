51ddda818855c972acf7fd2c815c30e2ce9cc90b
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2026-07-28 19:29:43 +0530

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    unordered_set<string>st;
+    int maxLen = -1;
+    int n;
+    bool solve(int idx, string &s, vector<int>& dp){
+        if(idx >= n) return true;
+        if(dp[idx] != -1) return dp[idx];
+        if(st.find(s.substr(idx)) != st.end()) return true;
+        for(int l = 1; l <= maxLen; l++){
+            string temp = s.substr(idx, l);
+            if(st.find(temp) != st.end() && solve(idx + l, s, dp)) return true;
+        }
+        return dp[idx] = false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        n = s.size();
+        vector<int>dp(s.size(), -1);
+        for(auto &x : wordDict){
+            st.insert(x);
+            maxLen = max(maxLen, int(x.size()));
+        }
+        return solve(0, s, dp);
+    }
+};
