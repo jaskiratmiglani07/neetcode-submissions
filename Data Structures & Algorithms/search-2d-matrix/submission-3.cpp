ef661491f9c4acd5814370cc7c30160a0d4f1e94
jaskiratmiglani07 <162829654+jaskiratmiglani07@users.noreply.github.com>
2026-09-09 17:47:03 +0530

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        // vector<int>mat;
+        // for(int i = 0; i < m; i++){
+        //     mat.insert(mat.end(), matrix[i].begin(), matrix[i].end());            
+        // }  
+        int lo = 0;
+        int hi =( m * n )- 1;  
+        while(lo <= hi){
+            int mid = lo + (hi - lo)/2;
+            if(matrix[mid / n][mid % n] == target){
+                return true;
+            }
+            if(matrix[mid / n][mid % n] > target) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return false;
+    }
+};
